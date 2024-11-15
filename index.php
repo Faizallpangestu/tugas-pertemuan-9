0133be3cf400cddd4ac70231d359e44b3e7eb694
Faizallpangestu <muhammadfaizal.mf10@gmail.com>
2024-11-15 22:21:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$nama   = "Muhammad Faizal";
+$Kelas  = "TI.23.CA.1";
+$Nim    = "312310551";
+$Prodi  = "Tekhnik Informatika";
+
+?>
+
+<h1>
+Saya Adalah 
+<?php echo $nama?>
+</h1>
+<p>
+ <strong>Kelas</strong>=<?php echo $Kelas?>
+</p>
+<p>
+ <strong>Nim</strong>=<?php echo $Nim?>
+</p>
+<p>
+<strong>Prodi</strong>=<?php echo $Prodi?>
+</p>
